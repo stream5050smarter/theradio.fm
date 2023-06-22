977c6e1573e3c262ddfa676071d77dbee7159353
stream5050smarter <stream.busniess.smarter@outlook.com>
2023-06-22 05:20:14 +0200

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,8 @@
+// Get necessary DOM elements
+const iframe = document.getElementById('website-iframe');
 
+// Open the iframe with the specified website URL
+openPopupButton.addEventListener('click', () => {
+  const websiteUrl = 'https://external-navigator.carrd.co'; // Replace with the desired website URL
+  iframe.src = websiteUrl;
+});
