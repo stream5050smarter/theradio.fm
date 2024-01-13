00874a175fe84982b38724e45eba20cb19e3081c
stream5050smarter <stream.busniess.smarter@outlook.com>
2024-01-14 01:55:38 +0200

    Update offlineFallbackPage path in sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ importScripts("https://clientcdn.pushengage.com/sdks/service-worker.js");
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "/offline.html";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
